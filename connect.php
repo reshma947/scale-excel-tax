87e66e45be208345ec42be3fe0c2a3b4b2dd602c
reshma947 <sk.reshma947@gmail.com>
2025-04-15 17:22:51 +0530

    First release of Hello World!

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "scale-excel";
+$dbname = "scale-excel-tax";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
